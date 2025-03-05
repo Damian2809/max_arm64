e9cbe999de5c2a0942a18727ebfd91a755594a7b
Damian2809 <lothk113@gmail.com>
2025-03-06 00:29:14 +0100

    uh

--- source/config.h
+++ source/config.h
@@ -1,20 +1,27 @@
-#ifndef CONFIG_H
-#define CONFIG_H
+#ifndef __CONFIG_H__
+#define __CONFIG_H__
+
+#define CONFIG_NAME "config.txt"
+#define MEMORY_MB 256
+#define SO_NAME "libmaxpayne.so"
+
+extern int screen_width;
+extern int screen_height;
 
 typedef struct {
-  int screen_width;
-  int screen_height;
-  int use_bloom;
-  int trilinear_filter;
-  int disable_mipmaps;
-  int language;
-  int crouch_toggle;
-  int character_shadows;
-  int drop_highest_lod;
-  int show_weapon_menu;
-  float decal_limit;
-  float debris_limit;
-  char mod_file[256];
+    int screen_width;
+    int screen_height;
+    int use_bloom;
+    int trilinear_filter;
+    int disable_mipmaps;
+    int language;
+    int crouch_toggle;
+    int character_shadows;
+    int drop_highest_lod;
+    int show_weapon_menu;
+    float decal_limit;
+    float debris_limit;
+    char mod_file[256];
 } Config;
 
 extern Config config;
@@ -22,4 +29,4 @@ extern Config config;
 int read_config(const char *file);
 int write_config(const char *file);
 
-#endif // CONFIG_H
+#endif // __CONFIG_H__
--- source/main.c
+++ source/main.c
@@ -1,20 +1,24 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include <string.h>
 #include <unistd.h>
 #include <sys/stat.h>
 #include <sys/mman.h>
-#include <stdio.h>
-#include <fcntl.h>
-#include <dlfcn.h> // For dynamic linking
+#include <dlfcn.h>
 
 #include "config.h"
 #include "util.h"
 #include "error.h"
+#include "so_util.h"
 #include "hooks.h"
 #include "imports.h"
 
 static void *heap_so_base = NULL;
 static size_t heap_so_limit = 0;
+
+// Screen dimensions, probably stupid because most devices are 640x480
+int screen_width = 1280;
+int screen_height = 720;
 
 static void check_data(void) {
     const char *files[] = {
@@ -26,17 +30,13 @@ static void check_data(void) {
         "x_level3.ras",
         "data",
         "es2",
-        // if this is missing, assets folder hasn't been merged in
         "es2/DefaultPixel.txt",
-        // mod file goes here
         "",
     };
     struct stat st;
     unsigned int numfiles = (sizeof(files) / sizeof(*files)) - 1;
-    // if mod is enabled, also check for mod file
     if (config.mod_file[0])
         files[numfiles++] = config.mod_file;
-    // check if all the required files are present
     for (unsigned int i = 0; i < numfiles; ++i) {
         if (stat(files[i], &st) < 0) {
             fatal_error("Could not find\n%s.\nCheck your data files.", files[i]);
@@ -47,7 +47,6 @@ static void check_data(void) {
 
 static void set_screen_size(int w, int h) {
     if (w <= 0 || h <= 0 || w > 1280 || h > 720) {
-        // auto; pick resolution based on display mode
         screen_width = 1280;
         screen_height = 720;
     } else {
@@ -58,47 +57,35 @@ static void set_screen_size(int w, int h) {
 }
 
 int main(void) {
-    // try to read the config file and create one with default values if it's missing
     if (read_config(CONFIG_NAME) < 0)
         write_config(CONFIG_NAME);
 
     check_data();
 
-    // calculate actual screen size
     set_screen_size(config.screen_width, config.screen_height);
 
     printf("heap size = %u KB\n", MEMORY_MB * 1024);
     printf(" lib base = %p\n", heap_so_base);
-    printf("  lib max = %u KB\n", heap_so_limit / 1024);
+    printf("  lib max = %zu KB\n", heap_so_limit / 1024);
 
-    // Load the shared library
     void *handle = dlopen(SO_NAME, RTLD_LAZY);
     if (!handle) {
         fatal_error("Could not load\n%s: %s", SO_NAME, dlerror());
     }
 
-    // won't save without it
     mkdir("savegames", 0777);
 
     update_imports();
 
-    // Resolve functions
-    uint32_t (*initGraphics)(void) = dlsym(handle, "_Z12initGraphicsv");
-    uint32_t (*ShowJoystick)(int show) = dlsym(handle, "_Z12ShowJoystickb");
-    int (*NVEventAppMain)(int argc, char *argv[]) = dlsym(handle, "_Z14NVEventAppMainiPPc");
-
-    if (!initGraphics || !ShowJoystick || !NVEventAppMain) {
-        fatal_error("Could not resolve required functions: %s", dlerror());
-    }
+    so_relocate();
+    so_resolve(dynlib_functions, dynlib_numfunctions, 1);
 
     patch_openal();
     patch_opengl();
     patch_game();
 
-    // can't set it in the initializer because it's not constant
     stderr_fake = stderr;
 
-    // Set up game-specific variables
     char *storageRootBuffer = dlsym(handle, "StorageRootBuffer");
     uint8_t *isAndroidPaused = dlsym(handle, "IsAndroidPaused");
     uint8_t *useRGBA8 = dlsym(handle, "UseRGBA8");
@@ -106,18 +93,25 @@ int main(void) {
     if (storageRootBuffer && isAndroidPaused && useRGBA8) {
         strcpy(storageRootBuffer, ".");
         *isAndroidPaused = 0;
-        *useRGBA8 = 1; // RGB565 FBOs suck
+        *useRGBA8 = 1;
     } else {
         fatal_error("Could not resolve game-specific variables: %s", dlerror());
     }
 
-    // Initialize graphics and start the game
+    uint32_t (*initGraphics)(void) = dlsym(handle, "_Z12initGraphicsv");
+    uint32_t (*ShowJoystick)(int show) = dlsym(handle, "_Z12ShowJoystickb");
+    int (*NVEventAppMain)(int argc, char *argv[]) = dlsym(handle, "_Z14NVEventAppMainiPPc");
+
+    so_finalize();
+    so_flush_caches();
+
+    so_execute_init_array();
+
+    so_free_temp();
+
     initGraphics();
     ShowJoystick(0);
     NVEventAppMain(0, NULL);
-
-    // Clean up
-    dlclose(handle);
 
     return 0;
 }
